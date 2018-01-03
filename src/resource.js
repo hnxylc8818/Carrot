ece4b3b1371561170bf1404669a546f4e8d5a0a5
xubinbin <xuzhibin212@vip.qq.com>
2018-01-03 19:14:05 +0800

    调整背景音乐

--- src/resource.js
+++ src/resource.js
@@ -1,9 +1,12 @@
 var res = {
+    // image
     HelloWorld_png : "res/HelloWorld.png",
     MMainScene_plist:"res/MainMenu/mainscene.plist",
     MMainScene_png:"res/MainMenu/mainscene.png",
     ROCKER_png:"res/Rocker/virtual_rocker_s.png",
     ROCKER_HANDLE_png:"res/Rocker/virtual_rocker_handle_s.png",
+
+    // music
     Main_BGM_mp3:"res/Audio/Music/main_bg.mp3"
 };
 
